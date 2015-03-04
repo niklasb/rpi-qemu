512bccea25933d73b6071538eeeab593c2bcd34a
Niklas Baumstark <niklas.baumstark@gmail.com>
2015-03-04 22:06:51 +0100

    add SSH script, comment run script

--- run.sh
+++ run.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-SSH_PORT=5022
-IFACE=eth0
+
 set -x
+SSH_PORT=5022
+
+# -redir listens on 0.0.0.0, so shut the port
 if ! sudo iptables -L -n | grep $SSH_PORT; then
   sudo iptables -A INPUT -p tcp -s 127.0.0.1 --dport $SSH_PORT -j ACCEPT
   sudo iptables -A INPUT -p tcp --dport $SSH_PORT -j DROP
 fi
+
 # We should use
 # -netdev user,id=user.0 -device e1000,netdev=user.0
-# BUT I don't know what device to use. Seems like QEMU implicitly uses
-# some kind of
+# But I don't know what device to use. The guest OS reports
+# that a SMC91C11xFD controller is implicitly used, but I can't figure out how
+# to select it explicitly, so that we can use  -netdev and -device
 qemu-system-arm \
   -cpu arm1176 \
   -m 256 \
---
+++ ssh.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh pi@localhost -p 5022
